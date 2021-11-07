3f15c76fed303f7ce3d9cb831d9a9cc095b7de9b
kaiserthe13th <superkerem13@gmail.com>
2021-11-07 22:59:04 +0300

    update autoscript.yml

--- __main__.py
+++ __main__.py
@@ -26,9 +26,11 @@ EXEC_TIME = D_EXEC_TIME
 D_SCRIPT_LOC = "autoscript.yml"
 SCRIPT_LOC = D_SCRIPT_LOC
 
+## Versioning Info
 __version__ = '2.0.0'
 __verlist__ = list(map(lambda x: int(x), __version__.split('.'))) # This turns version into a list of numbers (x, y, z being a number, x.y.z -> [x, y, z])
 __reldate__ = '7 November 2021' # Release Date
+
 __authors__ = ['Kerem Göksu <superkerem13@gmail.com>'] # Authors
 
 ## Templates
